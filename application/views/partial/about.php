7c0893d67ec717057b8c535d12f4678864398b15
agileup <agileup@gmail.com>
2014-05-27 16:59:26 +0900

    add "About" contents

--- application/views/partial/about.php
+++ application/views/partial/about.php
@@ -1,1 +1,39 @@
-<h1>Hi all</h1>
+<div class="about-container">
+	<h1 style="margin:0 0 15px 0;">Team LangHACK</h1>
+	<div id="carousel-about" class="carousel slide about" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#carousel-about" data-slide-to="0" class="active"></li>
+			<li data-target="#carousel-about" data-slide-to="1" class=""></li>
+			<li data-target="#carousel-about" data-slide-to="2" class=""></li>
+		</ol>
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner">
+			<div class="item active">
+				<img alt="First slide" src="/static/img/1_LangHack.JPG">
+			</div>
+			<div class="item">
+				<img alt="Second slide" src="/static/img/2_LangHack.JPG">
+			</div>
+			<div class="item">
+				<img alt="Third slide" src="/static/img/3_LangHack.JPG">
+			</div>
+		</div>
+		<!-- Controls -->
+		<a class="left carousel-control" href="#carousel-about" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+		</a>
+		<a class="right carousel-control" href="#carousel-about" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+		</a>
+	</div>
+	<p class="about-caption">Minki Choi (Korea)<span class="sep">|</span>Zac Durber (Australia)<span class="sep">|</span>George Song (USA)<span class="sep">|</span>YongHyun Cho (Korea)</p>
+	<div class="about-desc">
+		<p>We met each other on May 19, 2014 at the Global Hackathon in Dongdaemun Design Plaza in Seoul, Korea, sponsored by AppCenter and SBS Foundation. Within a span of less than 24 hours, we brainstormed, conceptualized and launched <strong>LangHack</strong>.</p>
+		<p><strong>LangHack</strong> is a platform that connects language learners with natives wherever they are.</p>
+		<p>Most language learning is one-sided, expensive, time consuming, and just not fun.</p>
+		<p>We believe learners should connect in person with real people, for a true language exchange, while building deeper relationships and creating meaningful and memorable experiences.</p>
+		<p>Don’t just memorize the language, but practice it while experiencing cultures, foods, sights and sounds from natives who know all the ins and outs.</p>
+		<p><strong>LangHack</strong>, bringing people around the world closer together.</p>
+	</div>
+</div>
